19ddb8f2cc200d5a12e2ef41d0b00fb13795a63d
marijose05 <mariajosemaciasherrera@gmail.com>
2025-07-07 16:50:56 -0400

    hoy

---
+++ Modulo 3/PHP/Ejemplos PHP/ejercicio3.php
@@ -0,0 +1,10 @@
+<?php
+$suma_imparaes=0;
+for ($num = 1; $num<=100;$num++) {
+    if($num% 2 !=0){
+        $suma_impares += $num;
+
+    }
+}
+ echo"la suma de los numeros imparesdel 1 al 100 es:$suma_impares\n";
+ ?>
